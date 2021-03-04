adc7bdcf09a1b386654aa2165cbb4eb4ef1d0393
Edex01 <jorasiac@gmail.com>
2021-03-04 17:56:07 +0530

    apixu API setup

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // const { Telegraf } = require('telegraf')
 const { Composer } = require('micro-bot')
+const apixu = require('apixu');
+const apixuClient = new apixu.Apixu({
+    apikey: '752fdebad4f0126557b99d7259680da5'
+});
 
 // const bot = new Telegraf(1658432370:AAFj1RxAaXwTi_gSB6oNbqJo-b0oCZw9b6k)
 const bot = new Composer
 
-
 // Rest Commands
-bot.start((ctx) => ctx.reply('Welcome'))
+bot.start((ctx) => ctx.reply('Welcome'));
+
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
