224e0c2287e562c918ed7c2ea07776bd8fd96eca
Edex01 <jorasiac@gmail.com>
2021-03-04 18:48:50 +0530

    BugFIx4

--- index.js
+++ index.js
@@ -2,29 +2,17 @@
 const { Composer } = require('micro-bot')
 const fetch = require('node-fetch');
 
-    apikey: '752fdebad4f0126557b99d7259680da5'
-
 // const bot = new Telegraf(1658432370:AAFj1RxAaXwTi_gSB6oNbqJo-b0oCZw9b6k)
 const bot = new Composer
 
 // Rest Commands
-bot.start((ctx) => ctx.reply('Welcome'));
-
-// bot.on('text', (ctx) => {
-//     let query = ctx.update.message.text; // get query as text
-//     apixuClient.current(query).then((current) => {
-//       return ctx.reply(
-//         `The current weather in ${query} is C: ${current.current.temp_c} F:${current.current.temp_f}`);
-//     }).catch((err) => {
-//       return ctx.reply('This city is not exists', err);
-//     });
-//   });
+bot.start((ctx) => ctx.reply('Welcome, I am your weather BOT '));
 
 bot.on('text', (ctx) => {
     let query = ctx.update.message.text;
     return fetch(`http://api.weatherstack.com/current?access_key=752fdebad4f0126557b99d7259680da5&query=${query}`, { method: 'POST', body: 'a=1' })
     .then(res => res.json()) // expecting a json response
-    .then(json => ctx.reply(json));
+    .then(json => ctx.reply(json.location.name));
 
 });
 
