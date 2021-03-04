461e7496c47c5329bc08dad8af2e19296396ea19
Edex01 <jorasiac@gmail.com>
2021-03-04 17:24:37 +0530

    Heroku setup

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const { Telegraf } = require('telegraf')
+// const { Telegraf } = require('telegraf')
+const { Composer } = require('micro-bot')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+// const bot = new Telegraf(1658432370:AAFj1RxAaXwTi_gSB6oNbqJo-b0oCZw9b6k)
+const bot = new Composer
+
+
+// Rest Commands
+// bot.start((ctx) => ctx.reply('Welcome'))
+// bot.help((ctx) => ctx.reply('Send me a sticker'))
+// bot.on('sticker', (ctx) => ctx.reply('👍'))
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+module.exports = bot
+// bot.launch()
+
+
+// radiant-garden-80462 APP_ID
+// https://radiant-garden-80462.herokuapp.com/
+// 1658432370:AAFj1RxAaXwTi_gSB6oNbqJo-b0oCZw9b6k  -- BOT TOKEN
